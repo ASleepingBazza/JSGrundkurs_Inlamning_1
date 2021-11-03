d4f02b457f24523586acaf92f74647b0bd38b612
ASleepingBazza <elias.e.fredriksson@gmail.com>
2021-11-03 15:32:35 +0100

    Moving tasks between current and finished works

--- js/main.js
+++ js/main.js
@@ -2,32 +2,32 @@ class TodoItem {
     constructor(desc, prio, dateAdded) {
         this.id = null;
         this.actionButton = null;
-        this.descriptionText = desc;
+        this.trashButton = null;
+        this.description = desc;
+        this.otherInfo = null;
+
         this.prio = prio;
-        this.finished = false;
         this.dateAdded = dateAdded;
         this.dateFinished = null;
+
+        this.finished = false;
     }
 }
 
 let todoList = [];
 
 window.onload = function () {
-    createTodoList();
+    createHardcodedTodoList();
 };
 
-function createTodoList() {
+function createHardcodedTodoList() {
     let todo1 = new TodoItem("Städa rummet.", "High", new Date());
     let todo2 = new TodoItem("Plugga.", "Mid", new Date());
     let todo3 = new TodoItem("Göra en grej.", "Low", new Date());
 
     todo2.finished = true;
 
-    todoList.push(todo1);
-    todoList.push(todo2);
-    todoList.push(todo3);
-
-    createHTML(todoList);
+    createHTML([todo1, todo2, todo3]);
 }
 
 function createHTML(list) {
@@ -35,44 +35,50 @@ function createHTML(list) {
     let containerFinishedTodo = document.getElementById("finished-tasks");
 
     for (let i = 0; i < list.length; i++) {
-        let actionButton = createActionButton(i);
-        let description = createDescription(list[i].descriptionText);
-        let otherInfo = createOtherInfo(
-            list[i].prio,
-            list[i].dateAdded,
-            list[i].dateFinished
-        );
-        list[i].id = i;
-        let trashButton = createTrashButton(i);
-
-        let task = document.createElement("div");
-        task.className = "task";
-        task.id = i;
-
-        task.appendChild(actionButton);
-        task.appendChild(description);
-        task.appendChild(otherInfo);
-        task.appendChild(trashButton);
-
+        let task = createTask(list[i], i);
         if (list[i].finished) {
-            actionButton.className = "undo-button";
-            actionButton.firstChild.className = "fas fa-times";
             containerFinishedTodo.appendChild(task);
         } else {
             containerCurrentTodo.appendChild(task);
         }
+        todoList.push(list[i]);
     }
 }
 
-function createTask(todoItem) {}
+function createTask(todoItem, id) {
+    todoItem.actionButton = createActionButton(id, todoItem.finished);
+    todoItem.description = createDescription(todoItem.description);
+    todoItem.otherInfo = createOtherInfo(
+        todoItem.prio,
+        todoItem.dateAdded,
+        todoItem.dateFinished
+    );
+    todoItem.id = id;
+    todoItem.trashButton = createTrashButton(id);
 
-function createActionButton(id) {
+    let task = document.createElement("div");
+    task.className = "task";
+    task.id = id;
+
+    task.appendChild(todoItem.actionButton);
+    task.appendChild(todoItem.description);
+    task.appendChild(todoItem.otherInfo);
+    task.appendChild(todoItem.trashButton);
+
+    return task;
+}
+
+function createActionButton(id, finished) {
     let b = document.createElement("button");
     b.addEventListener("click", () => toggleAction(id));
-
-    b.className = "complete-button";
     let i = document.createElement("i");
-    i.className = "fas fa-check";
+    if (finished) {
+        b.className = "undo-button";
+        i.className = "fas fa-times";
+    } else {
+        b.className = "complete-button";
+        i.className = "fas fa-check";
+    }
 
     b.appendChild(i);
     return b;
@@ -187,18 +193,39 @@ function toggleAction(id) {
     }
 
     if (task != null) {
-        if (task.finished) {
-            task = false;
+        console.log(task);
+        let containerCurrentTodo = document.getElementById("current-tasks");
+        let containerFinishedTodo = document.getElementById("finished-tasks");
+
+        let moveFromList = -1;
+        let moveToList = -1;
+        if (!task.finished) {
+            moveFromList = containerCurrentTodo;
+            moveToList = containerFinishedTodo;
+            task.finished = true;
+
+            task.actionButton.className = "undo-button";
+            task.actionButton.firstChild.className = "fas fa-times";
         } else {
-            task = true;
+            moveFromList = containerFinishedTodo;
+            moveToList = containerCurrentTodo;
+            task.finished = false;
+
+            task.actionButton.className = "complete-button";
+            task.actionButton.firstChild.className = "fas fa-check";
+        }
+
+        if (moveFromList != -1) {
+            for (let i = 0; i < moveFromList.children.length; i++) {
+                if (moveFromList.children[i].id == task.id) {
+                    moveToList.appendChild(moveFromList.children[i]);
+                }
+            }
         }
     }
 }
 
 function deleteTask(id) {
-    let containerCurrentTodo = document.getElementById("current-tasks");
-    let containerFinishedTodo = document.getElementById("finished-tasks");
-
     let task = null;
     for (t of todoList) {
         if (t.id == id) {
@@ -208,6 +235,8 @@ function deleteTask(id) {
     }
 
     if (task != null) {
+        let containerCurrentTodo = document.getElementById("current-tasks");
+        let containerFinishedTodo = document.getElementById("finished-tasks");
         let whichList = -1;
 
         if (task.finished) {
@@ -220,14 +249,9 @@ function deleteTask(id) {
             for (let i = 0; i < whichList.children.length; i++) {
                 if (whichList.children[i].id == task.id) {
                     whichList.removeChild(whichList.children[i]);
-                    console.log("ASDLKASDASD");
                 }
             }
-
             todoList.splice(todoList.indexOf(task), 1);
-            console.log("JS TODOLIST: ", todoList);
-            console.log("CURRENT: ", containerCurrentTodo.children);
-            console.log("FINISHED: ", containerFinishedTodo.children);
         }
     }
 }
