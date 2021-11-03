dd02dd018385ffb416ad0288866f59f59b0c3cb0
ASleepingBazza <elias.e.fredriksson@gmail.com>
2021-11-03 15:02:45 +0100

    Removing Tasks works

--- js/main.js
+++ js/main.js
@@ -1,53 +1,62 @@
 class TodoItem {
-    constructor(desc, prio, finished, dateAdded) {
+    constructor(desc, prio, dateAdded) {
+        this.id = null;
+        this.actionButton = null;
         this.descriptionText = desc;
         this.prio = prio;
-        this.finished = finished;
+        this.finished = false;
         this.dateAdded = dateAdded;
-        this.dateFinished = None;
-    }
-
-    complete() {
-        this.finished = true;
-        this.dateFinished = new Date();
+        this.dateFinished = null;
     }
 }
+
+let todoList = [];
 
 window.onload = function () {
     createTodoList();
 };
 
 function createTodoList() {
-    let todo1 = new TodoItem("Städa rummet.", "High", false, new Date());
-    let todo2 = new TodoItem("Plugga.", "Mid", true, new Date());
-    let todo3 = new TodoItem("Göra en grej.", "Low", false, new Date());
+    let todo1 = new TodoItem("Städa rummet.", "High", new Date());
+    let todo2 = new TodoItem("Plugga.", "Mid", new Date());
+    let todo3 = new TodoItem("Göra en grej.", "Low", new Date());
 
-    createTodoHTML([todo1, todo2, todo3]);
+    todo2.finished = true;
+
+    todoList.push(todo1);
+    todoList.push(todo2);
+    todoList.push(todo3);
+
+    createHTML(todoList);
 }
 
-function createTodoHTML(list) {
+function createHTML(list) {
     let containerCurrentTodo = document.getElementById("current-tasks");
     let containerFinishedTodo = document.getElementById("finished-tasks");
 
-    for (todo in list) {
-        let completeButton = createCompleteButton();
-        let description = createDescription(todo.descriptionText);
+    for (let i = 0; i < list.length; i++) {
+        let actionButton = createActionButton(i);
+        let description = createDescription(list[i].descriptionText);
         let otherInfo = createOtherInfo(
-            todo.prio,
-            todo.dateAdded,
-            todo.dateFinished
+            list[i].prio,
+            list[i].dateAdded,
+            list[i].dateFinished
         );
-        let trashButton = createTrashButton();
+        list[i].id = i;
+        let trashButton = createTrashButton(i);
 
         let task = document.createElement("div");
         task.className = "task";
+        task.id = i;
 
-        task.appendChild(completeButton);
+        task.appendChild(actionButton);
         task.appendChild(description);
         task.appendChild(otherInfo);
         task.appendChild(trashButton);
 
-        if (todo.finished) {
+        if (list[i].finished) {
+            actionButton.className = "undo-button";
+            actionButton.firstChild.className = "fas fa-times";
             containerFinishedTodo.appendChild(task);
         } else {
             containerCurrentTodo.appendChild(task);
@@ -55,15 +64,18 @@ function createTodoHTML(list) {
     }
 }
 
-function createCompleteButton() {
-    let b = document.createElement("button");
-    b.className = "complete-button";
+function createTask(todoItem) {}
 
+function createActionButton(id) {
+    let b = document.createElement("button");
+    b.addEventListener("click", () => toggleAction(id));
+
+    b.className = "complete-button";
     let i = document.createElement("i");
     i.className = "fas fa-check";
 
     b.appendChild(i);
-    return i;
+    return b;
 }
 
 function createDescription(descText) {
@@ -102,7 +114,7 @@ function createOtherInfo(todoPrio, todoDateAdded, todoDateFinished) {
     } else if (todoPrio == "High") {
         prioStatus.className = "prio-high";
     }
-    prioStatus.innerHTML = todoPrio;
+    prioStatus.innerHTML = " " + todoPrio;
 
     prio.appendChild(prioTitle);
     prio.appendChild(prioStatus);
@@ -110,31 +122,112 @@ function createOtherInfo(todoPrio, todoDateAdded, todoDateFinished) {
     // DATE ADDED
     let dateAdded = document.createElement("span");
     dateAdded.className = "date-added";
-    dateAdded.innerHTML = "Added: " + todoDateAdded.toDateString();
+
+    let date =
+        todoDateAdded.getDate() +
+        "/" +
+        todoDateAdded.getMonth() +
+        "/" +
+        todoDateAdded.getFullYear();
+    let time =
+        todoDateAdded.getHours() +
+        ":" +
+        todoDateAdded.getMinutes() +
+        ":" +
+        todoDateAdded.getSeconds();
+    dateAdded.innerHTML = "Added: " + date + " - " + time;
 
     // Add all
     d.appendChild(prio);
     d.appendChild(dateAdded);
 
     // IF a completed date exists
-    if (todoDateFinished != None) {
+    if (todoDateFinished != null) {
         let dateFinished = document.createElement("span");
         dateFinished.className = "date-finished";
-        dateFinished.innerHTML = "Finished: " + todoDateFinished.toDateString();
+        let date =
+            dateFinished.getDate() +
+            "/" +
+            dateFinished.getMonth() +
+            "/" +
+            dateFinished.getFullYear();
+        let time =
+            dateFinished.getHours() +
+            ":" +
+            dateFinished.getMinutes() +
+            ":" +
+            dateFinished.getSeconds();
+        dateFinished.innerHTML = "Added: " + date + " - " + time;
+
         d.appendChild(dateFinished);
     }
 
     return d;
 }
 
-function createTrashButton() {
+function createTrashButton(id) {
     let b = document.createElement("button");
+    b.addEventListener("click", () => deleteTask(id));
     b.className = "trash-button";
 
     let i = document.createElement("i");
     i.className = "fas fa-trash-alt";
 
     b.appendChild(i);
-
     return b;
+}
+
+function toggleAction(id) {
+    let task = null;
+    for (t of todoList) {
+        if (t.id == id) {
+            task = t;
+            break;
+        }
+    }
+
+    if (task != null) {
+        if (task.finished) {
+            task = false;
+        } else {
+            task = true;
+        }
+    }
+}
+
+function deleteTask(id) {
+    let containerCurrentTodo = document.getElementById("current-tasks");
+    let containerFinishedTodo = document.getElementById("finished-tasks");
+
+    let task = null;
+    for (t of todoList) {
+        if (t.id == id) {
+            task = t;
+            break;
+        }
+    }
+
+    if (task != null) {
+        let whichList = -1;
+
+        if (task.finished) {
+            whichList = containerFinishedTodo;
+        } else {
+            whichList = containerCurrentTodo;
+        }
+
+        if (whichList != -1) {
+            for (let i = 0; i < whichList.children.length; i++) {
+                if (whichList.children[i].id == task.id) {
+                    whichList.removeChild(whichList.children[i]);
+                    console.log("ASDLKASDASD");
+                }
+            }
+
+            todoList.splice(todoList.indexOf(task), 1);
+            console.log("JS TODOLIST: ", todoList);
+            console.log("CURRENT: ", containerCurrentTodo.children);
+            console.log("FINISHED: ", containerFinishedTodo.children);
+        }
+    }
 }
