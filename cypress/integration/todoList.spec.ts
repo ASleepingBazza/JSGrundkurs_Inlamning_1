70f78401c7a19fed20ae508c2a7fa2b9f1ff2a1d
ASleepingBazza <elias.e.fredriksson@gmail.com>
2021-11-24 16:00:24 +0100

    Added more cypress tests

--- cypress/integration/todoList.spec.ts
+++ cypress/integration/todoList.spec.ts
@@ -5,14 +5,14 @@ describe("Todo List Testing", () => {
         cy.visit("http://localhost:1234");
     });
 
-    it("should have 5 hard coded non-finished items by default when loading page", () => {
+    it("Should have 5 hard coded non-finished tasks by default when loading page", () => {
         //1. Arrange
         //2. Act
         //3. Asses
         cy.get("#current-tasks > div").should("have.length", 4);
     });
 
-    it("should have added one item to the non-finished tasks", () => {
+    it("Should have added one task", () => {
         //1. Arrange
         cy.get("#addTaskButton").click();
         cy.get("#taskDescription").type("Clean the garage.");
@@ -23,5 +23,164 @@ describe("Todo List Testing", () => {
 
         //3. Asses
         cy.get("#current-tasks > div").should("have.length", 5);
+    });
+
+    it("Should add one task and then mark it as done", () => {
+        //1. Arrange
+        cy.get("#addTaskButton").click();
+        cy.get("#taskDescription").type("Clean the garage.");
+        cy.get("#importance").select("Low");
+        cy.get("#confirmAdd").click();
+        cy.get("#current-tasks > div").should("have.length", 5);
+
+        //2. Act
+        cy.get(
+            "#current-tasks > div:nth-child(5) > button.complete-button"
+        ).click();
+
+        //3. Asses
+        cy.get("#current-tasks > div").should("have.length", 4);
+        cy.get("#finished-tasks > div").should("have.length", 1);
+    });
+
+    it("Should add one task, mark it as done and then mark it as not done", () => {
+        //1. Arrange
+        cy.get("#addTaskButton").click();
+        cy.get("#taskDescription").type("Clean the garage.");
+        cy.get("#importance").select("Low");
+        cy.get("#confirmAdd").click();
+        cy.get("#current-tasks > div").should("have.length", 5);
+        cy.get(
+            "#current-tasks > div:nth-child(5) > button.complete-button"
+        ).click();
+        cy.get("#current-tasks > div").should("have.length", 4);
+        cy.get("#finished-tasks > div").should("have.length", 1);
+
+        //2. Act
+        cy.get("#finished-tasks > div:first > .undo-button").click();
+
+        //3. Asses
+        cy.get("#current-tasks > div").should("have.length", 5);
+    });
+
+    it("Should add one task and then remove it", () => {
+        //1. Arrange
+        cy.get("#addTaskButton").click();
+        cy.get("#taskDescription").type("Clean the garage.");
+        cy.get("#importance").select("Low");
+        cy.get("#confirmAdd").click();
+        cy.get("#current-tasks > div").should("have.length", 5);
+
+        //2. Act
+        cy.get("#current-tasks > div:first > .trash-button").click();
+
+        //3. Asses
+        cy.get("#current-tasks > div").should("have.length", 4);
+    });
+
+    it("Should add one task, mark it as done and then remove it", () => {
+        //1. Arrange
+        cy.get("#addTaskButton").click();
+        cy.get("#taskDescription").type("Clean the garage.");
+        cy.get("#importance").select("Low");
+        cy.get("#confirmAdd").click();
+        cy.get("#current-tasks > div").should("have.length", 5);
+
+        //2. Act
+        cy.get("#current-tasks > div:first > .complete-button").click();
+        cy.get("#finished-tasks >div:first").should("have.length", 1);
+        cy.get("#finished-tasks > div:first > .trash-button").click();
+
+        //3. Asses
+        cy.get("#current-tasks > div").should("have.length", 4);
+        cy.get("#finished-tasks > div").should("have.length", 0);
+    });
+
+    it("Should change the sorting of current list to Low and sort accordingly", () => {
+        //1. Arrange
+        cy.get("#current-tasks > div").should("have.length", 4);
+
+        //2. Act
+        cy.get("#current-filter").select("Low");
+
+        //3. Asses
+        cy.get("#current-tasks > div:first > .other-info > .prio").should(
+            "have.text",
+            "Prio: Low"
+        );
+    });
+
+    it("Should change the sorting of current list to Mid and sort accordingly", () => {
+        //1. Arrange
+        cy.get("#current-tasks > div").should("have.length", 4);
+
+        //2. Act
+        cy.get("#current-filter").select("Mid");
+
+        //3. Asses
+        cy.get("#current-tasks > div:first > .other-info > .prio").should(
+            "have.text",
+            "Prio: Mid"
+        );
+    });
+
+    it("Should change the sorting of current list to High and sort accordingly", () => {
+        //1. Arrange
+        cy.get("#current-tasks > div").should("have.length", 4);
+
+        //2. Act
+        cy.get("#current-filter").select("High");
+
+        //3. Asses
+        cy.get("#current-tasks > div:first > .other-info > .prio").should(
+            "have.text",
+            "Prio: High"
+        );
+    });
+
+    it("Should add a new date when the task is marked as finished", () => {
+        //1. Arrange
+        cy.get("#addTaskButton").click();
+        cy.get("#taskDescription").type("Clean the garage.");
+        cy.get("#importance").select("Low");
+        cy.get("#confirmAdd").click();
+        cy.get("#current-tasks > div").should("have.length", 5);
+        cy.get(
+            "#current-tasks > div:nth-child(5) > button.complete-button"
+        ).click();
+        cy.get("#current-tasks > div").should("have.length", 4);
+        cy.get("#finished-tasks > div").should("have.length", 1);
+
+        //2. Act
+        //3. Asses
+        cy.get(
+            "#finished-tasks > div:first > .other-info > span:nth-child(3)"
+        ).should("have.length", 1);
+    });
+
+    it("Should remove the finished date when making a finished task as not finished", () => {
+        //1. Arrange
+        cy.get("#addTaskButton").click();
+        cy.get("#taskDescription").type("Clean the garage.");
+        cy.get("#importance").select("Low");
+        cy.get("#confirmAdd").click();
+        cy.get("#current-tasks > div").should("have.length", 5);
+        cy.get(
+            "#current-tasks > div:nth-child(5) > button.complete-button"
+        ).click();
+        cy.get("#current-tasks > div").should("have.length", 4);
+        cy.get("#finished-tasks > div").should("have.length", 1);
+        cy.get(
+            "#finished-tasks > div:first > .other-info > span:nth-child(3)"
+        ).should("have.length", 1);
+
+        //2. Act
+        cy.get("#finished-tasks > div:first > .undo-button").click();
+
+        //3. Asses
+        cy.get("#current-tasks > div:nth-child(4) > .other-info > span").should(
+            "have.length",
+            2
+        );
     });
 });
